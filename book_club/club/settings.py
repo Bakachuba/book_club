e273acb73b2f5ebad0cd182c93ffe187b4f2f7f0
Backachuba <phaktonlol@gmail.com>
2024-04-08 16:25:54 +0600

    deploying to aws v1.3

--- book_club/club/settings.py
+++ book_club/club/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -135,10 +135,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
