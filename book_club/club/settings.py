6026b6ba0cb45555143055db43374514ce79a601
Backachuba <phaktonlol@gmail.com>
2024-04-08 16:47:03 +0600

    deploying to aws v1.3

--- book_club/club/settings.py
+++ book_club/club/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'book_club/staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'book_club/static')
 
 
 # Default primary key field type
